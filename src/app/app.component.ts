2f1891da04d3f724fc92fcc16863527133716222
otaviovcn <viinident@gmail.com>
2024-12-21 17:44:52 -0300

    :sparkles:  feat - Cria filtro de contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +24,30 @@ import { ContatoComponent } from './componentes/contato/contato.component';
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   alfabeto: string = "abcdefghijklmnopqrstuvxywz";
+  contatos: Contato[] = agenda;
+  filtroPorTexto: string = '';
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  public filtrarContatosPorTexto(): Contato[] {
+    if(!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter(contato => this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto.toLowerCase())));
+  }
+
+  public filtrarContatos(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter(contato => contato.nome[0].toLowerCase() === letra);
+  }
+
 }
